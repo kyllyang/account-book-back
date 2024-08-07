4c7242738d3c2871e40ea9f67a57d1c042f2b462
yanglei <yanglei002@sinosoft.com>
2024-08-07 17:30:29 +0800

    项目初始化

---
+++ src/main/java/xyz/ksevo/accountbook/Application.java
@@ -0,0 +1,17 @@
+package xyz.ksevo.accountbook;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * User: Kyll
+ * Date: 2024-08-07 14:51
+ */
+@Slf4j
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
